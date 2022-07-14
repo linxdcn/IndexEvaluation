1170ff78275d87c1f2d1f93911e71b06aa37337a
linxdcn <linxdcn@gmail.com>
2022-07-14 21:45:55 +0800

    fix mac wind

--- evaluation/data.py
+++ evaluation/data.py
@@ -7,18 +7,18 @@ def get_index_table(index_code, start_date, end_date):
     start_str = start_date.strftime("%Y-%m-%d")
     end_str = end_date.strftime("%Y-%m-%d")
     error, df = w.wsd(index_code, "close,pe_ttm,pb_lf,dividendyield2", start_str, end_str, "", usedf=True)
-    df.dropna(inplace=True)
+    if start_str == end_str:
+        df['date'] = [start_date]
+        df.set_index(['date'], inplace=True)
     df.index.names = ['date']
-    df.rename(columns={'CLOSE':'close'},inplace=True)
-    df.rename(columns={'PE_TTM':'pe'},inplace=True)
-    df.rename(columns={'PB_LF':'pb'},inplace=True)
-    df.rename(columns={'DIVIDENDYIELD2':'dividend'},inplace=True)
-
+    df.columns = df.columns.map(lambda x : x.lower())
+    df.dropna(inplace=True)
     if df.empty:
         return df
     df = df.round(4)
     return df
 
 def get_index_name(index_code):
-    error, df = w.wss(index_code, "sec_name", usedf=True)
-    return df.SEC_NAME[index_code]
+    error, df = w.wss(index_code, "SEC_NAME", usedf=True)
+    df.columns = df.columns.map(lambda x : x.lower())
+    return df.sec_name[index_code]
